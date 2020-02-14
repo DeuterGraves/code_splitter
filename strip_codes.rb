78469e1bf73332e953f18f9f85a00e5adbda74af
Caroline Graves <caroline.graves@freeagent.com>
2020-02-14 23:31:30 +0000

    builds srt array and mp4 array

--- strip_codes.rb
+++ strip_codes.rb
@@ -3,7 +3,7 @@ def get_codes(strings_file)
   # strings_data
     # split into 2 arrays - 1 for .mp4s and the other for .srt
     mp4s = build_array(strings_data, "mp4")
-    mp4s
+    srts = build_array(strings_data, "srt")
   # compare arrays - keep the mp4 items that don't match .srt items
   # get the codes from the orphan mp4s
 end
