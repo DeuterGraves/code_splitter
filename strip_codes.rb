add1813a62a11f2044571328471b38e614a4597b
Caroline Graves <caroline.graves@freeagent.com>
2020-02-14 23:28:57 +0000

    makes mp4 array

--- strip_codes.rb
+++ strip_codes.rb
@@ -1,15 +1,20 @@
 def get_codes(strings_file)
   strings_data = File.read(strings_file).split
-  strings_data
-
+  # strings_data
     # split into 2 arrays - 1 for .mp4s and the other for .srt
-  strings_data.each do |string|
-    if string.split(".").last == "mp4"
-      puts "mp4"
-    else 
-      puts "srt"
-    end
-  end
+    mp4s = build_array(strings_data, "mp4")
+    mp4s
   # compare arrays - keep the mp4 items that don't match .srt items
   # get the codes from the orphan mp4s
+end
+
+def build_array(strings_data, extension)
+  array = []
+
+  strings_data.each do |string|
+    if string.split(".").last == extension
+      array << string
+    end
+  end 
+  array 
 end
