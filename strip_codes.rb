dcf9687baed7c01bff359eb8250a7257eae0ee98
Caroline Graves <caroline.graves@freeagent.com>
2020-02-15 00:42:49 +0000

    works on sample data set

--- strip_codes.rb
+++ strip_codes.rb
@@ -1,10 +1,11 @@
-def get_codes(strings_file)
+def get_orphan_codes(strings_file)
   strings_data = File.read(strings_file).split
   mp4s = build_array(strings_data, "mp4")
   srts = build_array(strings_data, "srt")
   # compare arrays - keep the mp4 items that don't match .srt items
   orphan_mp4s = mp4s - srts
   # get the codes from the orphan mp4s
+  get_codes(orphan_mp4s)
 end
 
 def build_array(strings_data, extension)
@@ -16,4 +17,13 @@ def build_array(strings_data, extension)
     end
   end 
   array 
+end
+
+def get_codes(orphan_mp4s)
+  codes = []
+  orphan_mp4s.each do |mp4| 
+    code = mp4.split("_")[-2]
+    codes << code
+  end
+  codes
 end
